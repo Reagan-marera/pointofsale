b751944ead0c8783ab1ad57d7eac7b50e2802dd1
reagan <marierareagan@gmail.com>
2025-09-08 19:15:48 +0300

    review

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'reaganx12x#'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///pos.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_PASSWORD = os.environ.get('SECRET_PASSWORD') or 'your_secret_password'
+    SECRET_PASSWORD = os.environ.get('SECRET_PASSWORD') or 'reaganx12x#'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
